3747b3c31a14c7ae0a8a498cce23255ad93d3534
nyrider <rideronthemorn@gmail.com>
2017-10-19 10:39:46 -0400

    Update main.tf

--- awsrubrik.org/main.tf
+++ awsrubrik.org/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = "us-west-2"
-  shared_credentials_file = "/Users/mil9186/.aws/credentials"
+  shared_credentials_file = "/.../.aws/credentials"
   profile                 = "default"
 }
 
